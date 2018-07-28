684675696253845b33ad1b7e74050dc2205666d5
Md.Khaled Hossain <md.khaled.eee@gmail.com>
2018-07-28 20:58:21 +0600

    Add media query by radium

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,8 +3,13 @@ import Radium from 'radium';
 import './Person.css';
 
 const person = (props) => {
+  const personStyle = {
+    '@media (min-width: 500px)': {
+      width: '450px'
+    }
+  }
   return (
-    <div className="Person">
+    <div className="Person" style={personStyle}>
       <p onClick={props.click}>I'm {props.name}, I'm {props.age} years old!. {props.children}</p>
       <input type="text" onChange={props.changeName} value={props.name}/>
     </div>
